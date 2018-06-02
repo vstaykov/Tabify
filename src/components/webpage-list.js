39684ba515d3da05689a0d98822110b75c1a5222
Ventsislav Staykov <vencislav.staykov@gmail.com>
2018-06-02 10:18:34 +0300

    Add webpages list component

---
+++ src/components/webpage-list.js
@@ -0,0 +1,49 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const WebPageList = props =>
+  props.webPages.map(webPage => {
+    const favIconUrl = `https://s2.googleusercontent.com/s2/favicons?domain_url=${
+      webPage.pageUrl
+    }/&amp;alt=s&amp;sz=16`;
+
+    let thumbnailElement = (
+      <div className="thumbnail-container default-thumbnail" />
+    );
+
+    if (webPage.thumbnailSrc !== "") {
+      thumbnailElement = (
+        <div className="thumbnail-container">
+          <image src="" />
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <div>
+          <img src={favIconUrl} title="" alt="" />
+          {webPage.title}
+        </div>
+        {thumbnailElement}
+        <div>
+          <i className="fa fa-trash" />
+          <label htmlFor="slider" className="switch">
+            <input id="slider" type="checkbox" checked="" />
+            <span className="slider round" />
+          </label>
+        </div>
+      </div>
+    );
+  });
+
+WebPageList.propTypes = {
+  webPages: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      pageUrl: PropTypes.string.isRequired
+    })
+  ).isRequired
+};
+
+export default WebPageList;
