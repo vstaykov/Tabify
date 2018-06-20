bf019a2f31b2a6ad74852463806d76d165ccf6d0
Ventsislav Staykov <vencislav.staykov@gmail.com>
2018-06-20 16:02:35 +0300

    Remove page URL thumbnail preview form options page

--- src/components/webpage-list.js
+++ src/components/webpage-list.js
@@ -7,25 +7,12 @@ const WebPageList = props =>
       webPage.pageUrl
     }/&amp;alt=s&amp;sz=16`;
 
-    let thumbnailElement = (
-      <div className="thumbnail-container default-thumbnail" />
-    );
-
-    if (webPage.thumbnailSrc && webPage.thumbnailSrc !== "") {
-      thumbnailElement = (
-        <div className="thumbnail-container">
-          <img src={webPage.thumbnailSrc} alt={webPage.pageUrl} />
-        </div>
-      );
-    }
-
     return (
       <div key={webPage.pageUrl}>
         <div>
           <img src={favIconUrl} title="" alt="" />
           {webPage.title}
         </div>
-        {thumbnailElement}
         <div>
           <i className="fa fa-trash" />
           <label htmlFor="slider" className="switch">
