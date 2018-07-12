716e42e538c011198b89fb8a073287dfe23dcdc0
Ventsislav Staykov <vencislav.staykov@gmail.com>
2018-07-12 22:59:29 +0300

    Update options page to save/retrieve web pages

--- src/components/options-page.js
+++ src/components/options-page.js
@@ -1,14 +1,42 @@
 import React from "react";
 import WebPageForm from "./webpage-form";
+import WebPageList from "./webpage-list";
+import WebPageStorageService from "./../utilities/webpage-storage-service";
 
-const OptionsPage = () => (
-  <div>
-    <WebPageForm
-      submit={(title, pageUrl) => {
-        console.log(`Title: ${title}; PageUrl: ${pageUrl}`);
-      }}
-    />
-  </div>
-);
+class OptionsPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { webPages: [] };
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  componentWillMount() {
+    this.updateWebPages();
+  }
+
+  handleFormSubmit(title, url, pinned) {
+    WebPageStorageService.saveWebPage(title, url, pinned).then(() => {
+      this.updateWebPages();
+    });
+  }
+
+  updateWebPages() {
+    WebPageStorageService.getWebPages().then(pages => {
+      if (pages !== undefined) {
+        this.setState({ webPages: pages });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <WebPageForm submit={this.handleFormSubmit} />
+        <WebPageList webPages={this.state.webPages} />
+      </div>
+    );
+  }
+}
 
 export default OptionsPage;
