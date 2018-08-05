64fae1a50731a105151077dddd2fadd50435898b
Ventsislav Staykov <vencislav.staykov@gmail.com>
2018-08-05 10:30:08 +0300

    Add delete web page logic in WebPageWebPageStorageService

--- src/utilities/webpage-storage-service.js
+++ src/utilities/webpage-storage-service.js
@@ -29,6 +29,26 @@ class WebPageStorageService {
 
     return setDataPromise;
   };
+
+  deleteWebPage = webPage => {
+    const deleteWebPagePromise = new Promise(resolve => {
+      this.getWebPages().then(webPages => {
+        const webPageIndex = webPages.findIndex(
+          wp =>
+            wp.pageUrl === webPage.pageUrl && wp.isPinned === webPage.isPinned
+        );
+
+        if (webPageIndex > -1) {
+          webPages.splice(webPageIndex, 1);
+          chrome.storage.sync.set({ tabifyWebPages: webPages }, result => {
+            resolve(result);
+          });
+        }
+      });
+    });
+
+    return deleteWebPagePromise;
+  };
 }
 
 export default WebPageStorageService;
