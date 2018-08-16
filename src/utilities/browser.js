e80a97a0daa803aab7150fd627efa9b277962a52
Ventsislav Staykov <vencislav.staykov@gmail.com>
2018-08-16 16:28:16 +0300

    Add Browser class

---
+++ src/utilities/browser.js
@@ -0,0 +1,44 @@
+class Browser {
+  constructor() {
+    this.chrome = chrome;
+  }
+
+  createTab = (url, pinned) => {
+    this.chrome.tabs.create({
+      url,
+      pinned
+    });
+  };
+
+  updateTab = (tab, data) => {
+    this.chrome.tabs.update(tab.id, data);
+  };
+
+  getTabs = query => {
+    const gatTabsPromise = new Promise(resolve => {
+      this.chrome.tabs.query(query, tabs => {
+        resolve(tabs);
+      });
+    });
+
+    return gatTabsPromise;
+  };
+
+  getStorageData = (key, resolveCallback) => {
+    this.chrome.storage.sync.get(key, result => {
+      resolveCallback(result);
+    });
+  };
+
+  setStorageData = (data, resolveCallback) => {
+    chrome.storage.sync.set(data, result => {
+      resolveCallback(result);
+    });
+  };
+
+  addCommandsListener = commandCallback => {
+    this.chrome.commands.onCommand.addListener(commandCallback);
+  };
+}
+
+export default Browser;
