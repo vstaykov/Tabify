b7d95f56f1408c0d5db103fef1ecbff38e563662
Ventsislav Staykov <vencislav.staykov@gmail.com>
2018-08-17 17:12:08 +0300

    Add Shortcut component tests

---
+++ test/components/shortcut.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Shortcut from "../../src/components/shortcut";
+import renderer from "react-test-renderer";
+
+describe("shortcut.js", () => {
+  describe("render()", () => {
+    describe("when given one key", () => {
+      it("render key correctly", () => {
+        const props = {
+          title: "Foo",
+          keys: ["Ctrl"]
+        };
+        const tree = renderer.create(<Shortcut {...props} />).toJSON();
+
+        expect(tree).toMatchSnapshot();
+      });
+    });
+
+    describe("when given several keys", () => {
+      it("render all keys correctly", () => {
+        const props = {
+          title: "Foo",
+          keys: ["Ctrl", "Shift", "Alt", "Q"]
+        };
+        const tree = renderer.create(<Shortcut {...props} />).toJSON();
+
+        expect(tree).toMatchSnapshot();
+      });
+    });
+  });
+});
