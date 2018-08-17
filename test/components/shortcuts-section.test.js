e76186f7093091b89449d70027d2ee12c41b4dc1
Ventsislav Staykov <vencislav.staykov@gmail.com>
2018-08-17 17:23:53 +0300

    Add ShortcutsSection component tests

---
+++ test/components/shortcuts-section.test.js
@@ -0,0 +1,45 @@
+import React from "react";
+import ShortcutsSection from "../../src/components/shortcuts-section";
+import renderer from "react-test-renderer";
+
+describe("shortcuts-section.js", () => {
+  describe("render()", () => {
+    describe("when given one shortcut", () => {
+      it("render shortcut correctly", () => {
+        const props = {
+         shortcuts: [{
+          title: "Foo",
+          keys: ["Ctrl"]
+         }]
+        };
+
+        const tree = renderer.create(<ShortcutsSection {...props} />).toJSON();
+
+        expect(tree).toMatchSnapshot();
+      });
+    });
+
+    describe("when given several shortcuts", () => {
+      it("render all shortcuts correctly", () => {
+        const props = {
+          shortcuts: [{
+           title: "Foo",
+           keys: ["Ctrl"]
+          },
+          {
+            title: "Bar",
+            keys: ["Alt", "Q"]
+          },
+          {
+            title: "FooBar",
+            keys: ["Ctrl", "Shift", "Alt", "Q"]
+          }]
+         };
+
+        const tree = renderer.create(<ShortcutsSection {...props} />).toJSON();
+
+        expect(tree).toMatchSnapshot();
+      });
+    });
+  });
+});
