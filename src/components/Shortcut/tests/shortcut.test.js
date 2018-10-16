016ca908356b3466a55871d99b21dda56cdfc33f
Ventsislav Staykov <vencislav.staykov@gmail.com>
2018-10-16 17:40:21 +0300

    Move shortcut component and tests under Shortcut folder

--- src/components/Shortcut/tests/shortcut.test.js
+++ src/components/Shortcut/tests/shortcut.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Shortcut from "../../src/components/shortcut";
+import Shortcut from "../shortcut";
 import renderer from "react-test-renderer";
 
 describe("<Shortcut />", () => {
--- src/components/shortcuts-section.js
+++ src/components/shortcuts-section.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Shortcut from "./shortcut";
+import Shortcut from "./Shortcut/shortcut";
 
 const ShortcutsSection = props => (
   <div className="shortcuts-section">
